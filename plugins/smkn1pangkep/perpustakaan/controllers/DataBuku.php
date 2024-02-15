5f2018efaa9e06c092ecb0e82f40bef5691bc23d
muhnasruddin <alpianganz4@gmail.com>
2024-02-15 09:15:55 +0700

    update rating buku

--- plugins/smkn1pangkep/perpustakaan/controllers/DataBuku.php
+++ plugins/smkn1pangkep/perpustakaan/controllers/DataBuku.php
@@ -1,6 +1,8 @@
 <?php namespace Smkn1Pangkep\Perpustakaan\Controllers;
 
 use Backend\Classes\Controller;
+use \Smkn1Pangkep\Perpustakaan\Models\UlasanBuku;
+use \Smkn1Pangkep\Perpustakaan\Models\Buku;
 use BackendMenu;
 
 class DataBuku extends Controller
@@ -18,5 +20,31 @@ class DataBuku extends Controller
     {
         parent::__construct();
         BackendMenu::setContext('Smkn1Pangkep.Perpustakaan', 'databuku');
+
+    }
+    public function onReloadRating(){
+        $this->hitungRating();
+        return $this->listRefresh();
+    }
+    public function hitungRating(){
+        $buku = Buku::get();
+        trace_log("hitung rating");
+        foreach($buku as $data){
+            $ulasan=UlasanBuku::where('buku_id', $data->id)->get();
+
+            $totalRating = 0;
+            $rating = 0;
+
+            foreach($ulasan as $u) {
+                $totalRating += $u->rating;
+                trace_log($u->rating);
+            }
+            trace_log($totalRating);
+            $rating = ($totalRating / count($ulasan))   ;
+            trace_log($rating);
+            $data->rating = $rating;
+            $data->save();
+
+        }
     }
 }
